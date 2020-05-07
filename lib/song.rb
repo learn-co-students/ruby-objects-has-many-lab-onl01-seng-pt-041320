d4ec90ea46f166e11b1524a8d3078ad21f6262fd
Andrew Alteri <andrewsalteri@gmail.com>
2020-05-07 14:45:04 -0500

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,19 @@
+class Song
+    attr_accessor :name, :artist
+
+    def initialize(name)
+        @name = name 
+        @@all << self
+    end
+
+    @@all = []
+
+    def self.all
+        @@all
+    end 
+
+    def artist_name 
+        self.artist == nil ? nil : self.artist.name
+    end
+
+end
