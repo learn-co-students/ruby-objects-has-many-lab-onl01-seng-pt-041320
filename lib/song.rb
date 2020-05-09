fbde8f642aa60d3882531d6b50f5a985ebd7f236
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2020-05-09 00:07:37 -0700

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,23 @@
+class Song
+    attr_accessor :artist, :name, :genre
+    
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @genre = genre
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def artist_name
+        if self.artist == nil
+            return nil
+        end
+        self.artist.name
+    end
+
+end
