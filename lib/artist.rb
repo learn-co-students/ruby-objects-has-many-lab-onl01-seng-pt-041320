2dc0c1e82979e99d38e98b55ed9b07a6bb411226
Philippe Timothe <philippetimothe@gmail.com>
2020-05-10 22:37:37 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,5 +1,5 @@
 class Artist 
-  attr_accessor :artist, :name, :songs
+  attr_accessor :name 
   
   def initialize(name)
     @name = name
@@ -7,12 +7,22 @@ class Artist
   end
   
   def add_song(song)
-      
+    @songs << song
+    song.artist = self
   end
   
+  def songs 
+    Song.all.select {|song| song.artist == self}
+  end
   
+  def add_song_by_name(name)
+    song = Song.new(name)
+    add_song(song)
+  end
   
-  
+  def self.song_count
+    Song.all.count
+  end
   
   
 end
