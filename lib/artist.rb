da6eacbea747fe967755b14eba4a3028101246c5
Philippe Timothe <philippetimothe@gmail.com>
2020-05-10 21:56:10 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,20 @@
+class Artist 
+  attr_accessor :artist, :name 
+  
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+  
+  def add_song(song)
+      
+  end
+  
+  def songs 
+    @songs
+  end
+  
+  
+  
+  
+end
---
+++ lib/song.rb
@@ -0,0 +1,22 @@
+class Song
+  attr_accessor :artist
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end 
+  
+  def name
+   @name
+  end
+
+  
+  
+  
+end 
