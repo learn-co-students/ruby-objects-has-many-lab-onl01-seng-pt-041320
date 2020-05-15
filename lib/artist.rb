f17eb34b4e216caf1124560cd4b5c9b47cc67cce
celestesmthomas <celestesmthomas@gmail.com>
2020-05-15 14:50:15 -0700

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,8 +1,6 @@
 class Artist
     attr_accessor :name
   
-    @@song_count = 0
-  
     def initialize(name)
       @name = name
       @songs = []
@@ -11,25 +9,22 @@ class Artist
     def add_song(song)
       @songs << song
       song.artist = self
-      @@song_count +=1
     end
 
     def add_song_by_name(name)
       song = Song.new(name)
       @songs << song
       song.artist = self
-      @@song_count +=1
     end
   
     def songs
-      artist_songs = @songs.all.select do |song| 
+      Song.all.select do |song| 
         song.artist == self
       end
-      artist_songs
     end
   
     def self.song_count
-      @@song_count
+      Song.all.count
   end
 
 end
